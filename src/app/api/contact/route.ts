682998ef91211b90ec63c0489851a8c6c75398ba
Premal2002 <114167095+Premal2002@users.noreply.github.com>
2025-02-24 02:29:40 +0530

    deleted unwanted files

--- src/app/api/contact/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  debugger;
-  const data = await req.json();
-  console.log("New Contact Message:", data);
-  return NextResponse.json({ message: "Message received" }, { status: 200 });
-}
